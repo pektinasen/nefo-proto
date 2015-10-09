b2ea77e620d45ba59eaaf39021c1210dcfe117f9
Sascha Gennrich <sascha.gennrich@gmail.com>
2015-10-09 12:33:13 +0200

    kss and kss:watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass'),
-    Eyeglass = require("eyeglass").Eyeglass;
+    kss = require('gulp-kss'),
+    Eyeglass = require('eyeglass').Eyeglass,
+    exec = require('child_process').exec;
 
 var eyeglass = new Eyeglass({
     includePaths: require('node-bourbon').includePaths,
@@ -16,25 +18,29 @@ var eyeglass = new Eyeglass({
 eyeglass.enableImportOnce = false
 
 // define tasks here
-gulp.task('default', function(){
-    gulp.src("./src/sass/style.scss")
+gulp.task('sass', function(){
+    gulp.src("./src/sass/**/*.scss")
         .pipe(sass(eyeglass.sassOptions()).
             on('error', sass.logError))
         .pipe(gulp.dest('./css'));
 });
-//
-//gulp.task('sass:watch', function () {
-//  gulp.watch('./src/sass/!**!/!*.scss', ['sass']);
-//});
-//
-//gulp.task('kss', function () {
-//  gulp.src('css/style.css')
-//      .pipe(kss({
-//          templateDirectory: 'nefo-kss-template/'
-//      }))
-//      .pipe(gulp.dest('styleguide'));
-//});
-//
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('kss', function(cb){
+    exec('npm run kss', function (err, stdout, stderr) {
+        console.log(err);
+        console.log(stdout);
+        console.log(stderr);
+    });
+});
+
+gulp.task('kss:watch', function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass', 'kss']);
+});
+
 //gulp.task('default', function () {
 //  //nothing here yet
 //  console.log(gulp);
