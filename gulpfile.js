ce82754177661aed746e2c84a3b78420a983c0c6
Sascha Gennrich <sascha.gennrich@gmail.com>
2015-10-09 10:34:03 +0200

    working taskrunner and dependencymanager with eyeglass

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,52 @@
 'use strict'
 
 var gulp = require('gulp');
+var sass = require('gulp-sass'),
+    Eyeglass = require("eyeglass").Eyeglass;
 
-var changed = require('gulp-changed'),
-	clean = require('gulp-clean'),
-	concat = require('gulp-concat'),
-    jshint = require ('gulp-jshint'),
-    kss = require('gulp-kss'),
-    minify = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify');
-
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+var eyeglass = new Eyeglass({
+    includePaths: require('node-bourbon').includePaths,
+    importer: function(uri, prev, done) {
+        done(sass.compiler.types.NULL);
+    }
 });
 
+// Disable import once with gulp until we
+// figure out how to make them work together.
+eyeglass.enableImportOnce = false
 
-gulp.task('default', function () {
-  //nothing here yet
-  console.log(gulp);
+// define tasks here
+gulp.task('default', function(){
+    gulp.src("./src/sass/style.scss")
+        .pipe(sass(eyeglass.sassOptions()).
+            on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
 });
-
-gulp.task('scripts', function() {
-  return gulp.src('./src/js/*.js')
-    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist/js/'));
-})
+//
+//gulp.task('sass:watch', function () {
+//  gulp.watch('./src/sass/!**!/!*.scss', ['sass']);
+//});
+//
+//gulp.task('kss', function () {
+//  gulp.src('css/style.css')
+//      .pipe(kss({
+//          templateDirectory: 'nefo-kss-template/'
+//      }))
+//      .pipe(gulp.dest('styleguide'));
+//});
+//
+//gulp.task('default', function () {
+//  //nothing here yet
+//  console.log(gulp);
+//});
+//
+//gulp.task('scripts', function() {
+//  return gulp.src('./src/js/!*.js')
+//    .pipe(concat('app.js'))
+//    .pipe(gulp.dest('./dist/js/'))
+//    .pipe(uglify())
+//    .pipe(rename({
+//      suffix: '.min'
+//    }))
+//    .pipe(gulp.dest('./dist/js/'));
+//})*/
